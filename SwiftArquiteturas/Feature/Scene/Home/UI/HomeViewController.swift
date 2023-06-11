ccaf2323dafb7a4e4f39cfa5254b4d81a3072f92
Thiago <Thiago Monteiro>
2023-06-10 22:18:11 -0300

    Created view

--- SwiftArquiteturas/Feature/Scene/Home/UI/HomeViewController.swift
+++ SwiftArquiteturas/Feature/Scene/Home/UI/HomeViewController.swift
@@ -1,67 +1,37 @@
 import UIKit
 
 protocol HomeDisplayLogic: AnyObject {
-  func displaySomething(viewModel: Home.Something.ViewModel)
+    func displaySomething(viewModel: Home.Something.ViewModel)
 }
 
-class HomeViewController: UIViewController, HomeDisplayLogic {
-  var interactor: HomeBusinessLogic?
-  var router: (NSObjectProtocol & HomeRoutingLogic & HomeDataPassing)?
-
-  // MARK: Object lifecycle
-  
-  override init(nibName nibNameOrNil: String?, bundle nibBundleOrNil: Bundle?) {
-    super.init(nibName: nibNameOrNil, bundle: nibBundleOrNil)
-    setup()
-  }
-  
-  required init?(coder aDecoder: NSCoder) {
-    super.init(coder: aDecoder)
-    setup()
-  }
-  
-  // MARK: - Setup
-  private func setup() {
-    let viewController = self
-    let interactor = HomeInteractor()
-    let presenter = HomePresenter()
-    let router = HomeRouter()
-    viewController.interactor = interactor
-    viewController.router = router
-    interactor.presenter = presenter
-    presenter.viewController = viewController
-    router.viewController = viewController
-    router.dataStore = interactor
-  }
-  
-  // MARK: Routing
-  
-  override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
-    if let scene = segue.identifier {
-      let selector = NSSelectorFromString("routeTo\(scene)WithSegue:")
-      if let router = router, router.responds(to: selector) {
-        router.perform(selector, with: segue)
-      }
+final class HomeViewController: UIViewController {
+    //MARK: - Properties
+    var interactor: HomeBusinessLogic?
+    var router: (NSObjectProtocol & HomeRoutingLogic & HomeDataPassing)?
+    
+    //MARK: - Life Cicle
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        view.backgroundColor = .systemMint
     }
-  }
-  
-  // MARK: View lifecycle
-  
-  override func viewDidLoad() {
-    super.viewDidLoad()
-    doSomething()
-  }
-  
-  // MARK: Do something
-  
-  //@IBOutlet weak var nameTextField: UITextField!
-  
-  func doSomething() {
-    let request = Home.Something.Request()
-    interactor?.doSomething(request: request)
-  }
-  
-  func displaySomething(viewModel: Home.Something.ViewModel) {
-    //nameTextField.text = viewModel.name
-  }
+    
+    override func viewWillAppear(_ animated: Bool) {
+        super.viewWillAppear(animated)
+        doSomething()
+    }
+    
+    //MARK: - Methods
+    private func doSomething() {
+        let request = Home.Something.Request()
+        interactor?.doSomething(request: request)
+    }
+    
+    //MARK: - Selectors
+}
+
+//MARK: - HomeDisplayLogic
+extension HomeViewController: HomeDisplayLogic {
+    func displaySomething(viewModel: Home.Something.ViewModel) {
+        
+    }
 }
